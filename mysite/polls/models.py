475b52ee6fb5cac5ccf9cb20d255a13836687d57
Kollawat Rupanya <micky2smart1@gmail.com>
2022-09-20 14:36:14 +0700

    vote property

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -34,7 +34,13 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+    # votes = models.IntegerField(default=0)
+    
+    @property
+    def votes(self):
+        """Return votes to that choice."""
+        return Vote.objects.filter(choice=self).count()
+
 
     def __str__(self):
         """Returns choices."""
