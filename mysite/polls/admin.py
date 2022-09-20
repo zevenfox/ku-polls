e025f7bb2d283a700a5dc42125ef5d1f0b0d25fc
Kollawat Rupanya <micky2smart1@gmail.com>
2022-09-21 00:27:34 +0700

    fix admin page

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-
-from .models import Choice, Question
-
+# Register your models here.
+from .models import Question, Choice, Vote
+  
+# admin.site.register(Question)
+# admin.site.register(Choice)
+  
+admin.site.site_header = "KU Poll Admin"
+admin.site.site_title = "Admin Area"
+admin.site.index_title = "Welcome to the KU Poll Admin Area"
+  
+  
 admin.site.register(Question)
 admin.site.register(Choice)
-
+admin.site.register(Vote)
 
